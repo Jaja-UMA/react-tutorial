fe755a4b04f4576ed37692a4d49cb2c5e58ba325
Jaja-UMA <westakr24@gmail.com>
2023-07-03 12:07:42 +0900

    Proceed tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
+import {List} from "./List";
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [descriotion,setDescription] = useState('クリック前の表示');
+
+  const changeDescriontion= () =>{
+    setDescription('クリック後の表示')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {descriotion}{}
+      <List title="まみむめも音楽"/>{/* */}
+      <button onClick={changeDescriontion}>ボタン</button>
     </div>
   );
 }
